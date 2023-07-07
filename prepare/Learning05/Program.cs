ea52ff60a1f4db74dc5000e1a970be303a80ba0e
e-farnsworth <farnsworth3e@gmail.com>
2023-07-06 18:56:54 -0600

    Preperation for overried  (shapes)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Purple", 5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Red", 2.6, 3.25);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Blue", 7.2);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine("");
+        }
+
+
     }
 }
